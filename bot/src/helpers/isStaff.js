1ba688908a5536decd27ac4326be8a1050668727
arte-trimbauser <arteompotrimba71@gmail.com>
2026-07-02 19:12:19 +0100

    Update isStaff.js

--- bot/src/helpers/isStaff.js
+++ bot/src/helpers/isStaff.js
@@ -1,6 +1,6 @@
 const config = require("../config");
 
 module.exports = function isStaff(member) {
-    if (!member || !member.roles || !member.roles.cache) return false;
-    return config.STAFF_ROLES.some(roleId => member.roles.cache.has(roleId));
+    if (!member || !member.roles) return false;
+    return config.STAFF_ROLES.some(id => member.roles.cache.has(id));
 };
