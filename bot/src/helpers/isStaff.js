69ba22d83183cf3410a52e040f8fb3714e350dbf
arte-trimbauser <arteompotrimba71@gmail.com>
2026-08-28 16:05:29 +0100

    Delete bot directory

--- bot/src/helpers/isStaff.js
+++
@@ -1,6 +0,0 @@
-const config = require("../config");
-
-module.exports = function isStaff(member) {
-    if (!member || !member.roles) return false;
-    return config.STAFF_ROLES.some(id => member.roles.cache.has(id));
-};
